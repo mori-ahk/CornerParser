3e41bea884ebc16e2418d441c6a450b6c468a4d5
Mori Ahmadi <ahmadik.morteza95@gmail.com>
2024-09-05 15:30:12 -0400

    Add documentation to public API of parser

--- Sources/CornerParser/Parser/ASTNode.swift
+++ Sources/CornerParser/Parser/ASTNode.swift
@@ -7,21 +7,49 @@
 
 import Foundation
 
+/// An enum representing an Abstract Syntax Tree (AST) node used to construct diagrams and nodes.
+///
+/// The `ASTNode` enum encapsulates the structure of a parsed diagram or individual node. It supports both `diagram` nodes,
+/// which can contain children nodes, and `node` nodes, which contain individual node declarations. Additionally, `ASTNode`
+/// defines the attributes and structure of nodes and edges used in diagrams.
+///
 public enum ASTNode: Equatable {
+    /// Represents a root diagram node that contains other AST nodes as children.
     case diagram(children: [ASTNode])
+    
+    /// Represents an individual node with its own declaration details encapsulated in ``NodeDecl``.
     case node(NodeDecl)
     
+    /// Represents an attribute that can be applied to a node in a diagram.
     public enum NodeAttribute: Equatable {
-       case color(String)
+        /// Specifies the color of the node, represented as a `String`.
+        case color(String)
     }
     
+    /// Represents an attribute that can be applied to an edge between nodes.
     public enum EdgeAttribute: Equatable {
+        /// Specifies a label for the edge, represented as a `String`.
         case label(String)
     }
     
+    /// A structure that represents a node declaration, encapsulating the node's details such as its identifier,
+    /// optional attributes, and any connected edges.
+    ///
+    /// # Example
+    /// ```swift
+    /// let node = ASTNode.NodeDecl(id: "A", attribute: .color("red"))
+    /// print(node.id) // "A"
+    /// print(node.attribute) // Optional(.color("red"))
+    /// ```
+    ///
     public struct NodeDecl: Equatable {
+        /// The unique identifier for the node.
         public let id: String
+        
+        /// An optional attribute applied to the node, such as color.
         public let attribute: NodeAttribute?
+        
+        /// An array of edges connected to this node.
         public let edges: [EdgeDecl]
         
         init(id: String, attribute: NodeAttribute? = nil, edges: [EdgeDecl] = []) {
@@ -31,9 +59,24 @@ public enum ASTNode: Equatable {
         }
     }
     
+    /// A structure representing an edge between two nodes in a diagram, including optional attributes such as a label.
+    ///
+    /// # Example
+    /// ```swift
+    /// let edge = ASTNode.EdgeDecl(from: "A", to: "B", attributes: [.label("AB Connection")])
+    /// print(edge.from) // "A"
+    /// print(edge.to) // "B"
+    /// print(edge.attributes) // [.label("AB Connection")]
+    /// ```
+    ///
     public struct EdgeDecl: Equatable {
+        /// The identifier of the starting node.
         public let from: String
+       
+        /// The identifier of the target node.
         public let to: String
+        
+        /// An array of attributes applied to the edge, such as a label.
         public let attributes: [EdgeAttribute]
         
         init(from: String, to: String, attributes: [EdgeAttribute] = []) {
@@ -43,6 +86,21 @@ public enum ASTNode: Equatable {
         }
     }
     
+    /// A Boolean property indicating whether the current `ASTNode` is a `node` or not.
+    ///
+    /// - Returns:
+    ///   - `true` if the current node is of type `.node`.
+    ///   - `false` if the current node is of type `.diagram`.
+    ///
+    /// # Example
+    /// ```swift
+    /// let node = ASTNode.node(ASTNode.NodeDecl(id: "A"))
+    /// print(node.isNode) // true
+    ///
+    /// let diagram = ASTNode.diagram(children: [])
+    /// print(diagram.isNode) // false
+    /// ```
+    ///
     public var isNode: Bool {
         switch self {
         case .node:
--- Sources/CornerParser/Parser/Parser.swift
+++ Sources/CornerParser/Parser/Parser.swift
@@ -16,18 +16,38 @@ class Parser {
         self.currentToken = lexer.nextToken()
     }
     
-    private func advance() {
-        currentToken = lexer.nextToken()
-    }
-    
-    private func expect(_ expectedToken: Token) throws {
-        if currentToken.token == expectedToken {
-            advance()
-        } else {
-            throw ParseError.unexpectedToken(expected: expectedToken, found: currentToken)
-        }
-    }
-    
+    /// Parses a sequence of tokens to generate an Abstract Syntax Tree (AST) representation.
+    ///
+    /// This method iterates through the tokens in the input, processing each one according to its type. The `parse()` method
+    /// expects a specific token type (`.node`) at each step, and will throw an error if an unexpected token is encountered.
+    /// Once all tokens are parsed, it returns a root `ASTNode` of type `.diagram`, containing all the parsed nodes as children.
+    ///
+    /// - Throws: `ParseError.unexpectedToken` when a wrong token other than is encountered during parsing. The error provides
+    ///      the expected token and the actual token that was found.
+    ///
+    /// - Returns:
+    ///    An `ASTNode` representing the root of the diagram. The root node has an array of child nodes representing
+    ///    the parsed elements.
+    ///
+    /// # Example
+    /// ```swift
+    /// let parser = Parser(lexer: Lexer(input: input))
+    /// let ast = try parser.parse()
+    /// print(ast) // ASTNode.diagram with parsed children nodes
+    /// ```
+    ///
+    /// # Discussion
+    /// This method forms the core of the parsing process by building up the tree structure of the diagram. It continually adds
+    /// ``ASTNode`` elements to the tree until it reaches the end of the token stream. The parsing process is essential for
+    /// transforming a tokenized input into a structure that can later be rendered or interpreted.
+    ///
+    /// - Important:
+    ///    The function assumes that `.eof` (end-of-file) tokens signal the end of the input. Therefore, all valid token streams
+    ///    must be terminated by an `.eof` token, or parsing will fail.
+    ///
+    /// - See Also:
+    ///   - ``ASTNode``: The structure used to represent nodes in the abstract syntax tree.
+    ///   
     func parse() throws -> ASTNode {
         var elements: [ASTNode] = []
         while currentToken.token != .eof {
@@ -40,6 +60,18 @@ class Parser {
         }
         
         return .diagram(children: elements)
+    }
+    
+    private func advance() {
+        currentToken = lexer.nextToken()
+    }
+    
+    private func expect(_ expectedToken: Token) throws {
+        if currentToken.token == expectedToken {
+            advance()
+        } else {
+            throw ParseError.unexpectedToken(expected: expectedToken, found: currentToken)
+        }
     }
     
     private func parseNode() throws -> ASTNode.NodeDecl {
